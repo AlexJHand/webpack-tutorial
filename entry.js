b4899cf46fd292dfaddba96d9263d5b74e58f917
Alex Hand <alex.j.hand@gmail.com>
2017-12-07 16:17:18 -0600

    Added css loader

--- entry.js
+++ entry.js
@@ -1,1 +1,7 @@
+// By prefixing loaders to a module request, the module went through a loader pipeline. 
+// These loaders transform the file content in specific ways. 
+// After these transformations are applied, the result is a JavaScript module.
+
+require("!style-loader!css-loader!./style.css");
+
 document.write(require("./content"));
